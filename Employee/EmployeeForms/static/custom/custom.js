d668fdc6086829c1191470d2cf15116f7656a527
Sujay Suresh <sujay.suresh@ansrsource.com>
2016-12-22 20:13:59 +0530

    reverting css and js files

--- Employee/EmployeeForms/static/custom/custom.js
+++ Employee/EmployeeForms/static/custom/custom.js
@@ -1,55 +1,5 @@
 // JavaScript Document
 jQuery(document).ready(function($){
 	//Put Your Custom Jquery or Javascript Code Here
-
-
-	// Height script for text section
-
-
-	heightFunction = function(){
-		hgt1=$('.jsh1').css('height');
-		$('.jsh2').css('height', hgt1);
-	}
-
-	// Height for navbar
-
-	navHeight = function(){
-		vph = $(window).height();
-		// console.log(vph);
-		// vphp = parseInt(vph);
-		vph -= 106;
-		// console.log(vphp);
-		$('.vd_content-wrapper').css('min-height', vph + 'px' );
-	}
-
-
-	extendRibbon = function(){
-		$('#oneteam-ribbon').addClass('extended');
-	}
-
-
-	// Function Calls
-
-
-	navHeight();
-	heightFunction();
-
-
-	$(window).resize(function(){
-		navHeight();
-		heightFunction();
-	})
-
-	$(window).load(function(){
-		heightFunction();
-		setTimeout(function(){ 
-			extendRibbon(); 
-		}, 1000);
-	})
-
-
-
-
-
 	
 });
