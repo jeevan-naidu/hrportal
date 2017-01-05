6119c795b7bb49bc4e7f2acbca9d9ba50cda89cf
Sujay Suresh <sujay.suresh@ansrsource.com>
2017-01-05 10:38:05 +0530

    deleting migrations

--- Employee/Employee/settings.py
+++ Employee/Employee/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "candidate",
         "USER": "root",
-        "PASSWORD": "root",
+        "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "3306",
     },
